e273781509544f6f2b3a171b4e42881a7222a97e
LFPires1993 <lfpires1993@gmail.com>
2024-03-15 15:11:12 -0300

    Create initial structure from project

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.txt}>
+        Welcome to Lucas Pires Calculator
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  txt: {
+    fontSize: 25
+  }
+});
