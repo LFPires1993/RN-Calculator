eeef4c3b4433fe1d09811e8543d58fcdb921e315
LFPires1993 <lfpires1993@gmail.com>
2024-03-16 15:25:37 -0300

    Create Display Component

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { SafeAreaView } from 'react-native';
 import styles from './src/styles';
 import Keyboard from './src/components/Keyboard';
+import Display from './src/components/Display';
 
 export default function App() {
+  state = {
+    displayNumber: '0'
+  }
   return (
     <SafeAreaView style={styles.app}>
+      <Display value={this.state.displayNumber}/>
       <Keyboard/>
     </SafeAreaView>
   );
